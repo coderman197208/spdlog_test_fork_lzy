abf8ea6765553416b78438042ba5ebddacca23ad
LZY <leatrices@163.com>
2025-11-27 16:10:35 +0800

    初始提交

---
+++ build/subproject1/CMakeFiles/subproject1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subproject1.dir/src/1_basic_include.cpp.obj"
+  "CMakeFiles/subproject1.dir/src/1_basic_include.cpp.obj.d"
+  "E:/Code/vscode_Projects/spdlog_test/bin/subproject1.exe"
+  "E:/Code/vscode_Projects/spdlog_test/bin/subproject1.exe.manifest"
+  "E:/Code/vscode_Projects/spdlog_test/bin/subproject1.pdb"
+  "libsubproject1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subproject1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ subproject1/src/1_basic_include.cpp
@@ -0,0 +1,8 @@
+// 1_basic_include.cpp
+#include <spdlog/spdlog.h>
+
+int main() {
+    // 最简单的使用方式
+    spdlog::info("Hello from spdlog!");
+    return 0;
+}
